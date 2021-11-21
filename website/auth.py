e0fe814ad8f823367d7902fa0ed799e65785deb9
Mohamed Sharif <msharabdq@gmail.com>
2021-11-21 21:22:41 +0000

    Added validation for login and signup

--- website/auth.py
+++ website/auth.py
@@ -9,12 +9,12 @@ from flask import Blueprint, render_template
  
 auth = Blueprint('auth', __name__)
  
-@auth.route('/signup')
+@auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
     return render_template("signup.html")
  
  
-@auth.route("/login")
+@auth.route("/login", methods = ['GET', 'POST'])
 def login():
     return render_template("login.html")
  
--- website/static/js/signup.js
+++ website/static/js/signup.js
@@ -1,15 +1,35 @@
 function show(pwd) {
-  var password=document.getElementById(pwd);
-  var btn=password.nextElementSibling
-  if (password.getAttribute('type') == "password") {
-  btn.removeAttribute("class");
-  btn.setAttribute("class","far fa-eye");
-  password.removeAttribute("type");
-    password.setAttribute("type","text");
-  } else {
-  password.removeAttribute("type");
-    password.setAttribute('type','password');
- btn.removeAttribute("class");
-  btn.setAttribute("class","far fa-eye-slash");
-  }
+    var password = document.getElementById(pwd);
+    var btn = password.nextElementSibling
+    if (password.getAttribute('type') == "password") {
+        btn.removeAttribute("class");
+        btn.setAttribute("class", "far fa-eye");
+        password.removeAttribute("type");
+        password.setAttribute("type", "text");
+    } else {
+        password.removeAttribute("type");
+        password.setAttribute('type', 'password');
+        btn.removeAttribute("class");
+        btn.setAttribute("class", "far fa-eye-slash");
+    }
+}
+
+// var paswd = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,30}$/;
+function validate(e) {
+    var pwd = document.getElementById("pwd").value;
+    if (pwd.length < 8) {
+        alert("Password not long enough");
+        return false;
+    } else if (pwd.search(/[a-z]/) < 0) {
+        alert("Your password needs a lowercase letter");
+        return false;
+    } else if (pwd.search(/[A-Z]/) < 0) {
+        alert("Your password needs an uppercase letter");
+        return false;
+    } else if (pwd.search(/[0-9]/) < 0) {
+        alert("Your password requires a number. ");
+        return false;
+    } else {
+        return true;
+    }
 }
